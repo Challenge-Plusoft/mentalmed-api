e81e5116a5a32cd5a9d1f2c954dc7f2321ebb7c9
Vinícius Yoda <vyoda4604@gmail.com>
2023-04-26 18:18:36 -0300

    projeto incial

---
+++ mentalmed/src/main/java/br/com/fiap/mentalmed/MentalmedApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mentalmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalmedApplication.class, args);
+	}
+
+}
---
+++ mentalmed/src/test/java/br/com/fiap/mentalmed/MentalmedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.mentalmed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalmedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
